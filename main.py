a0a04cf82c4abba7188f371d0af811ccd447a4c6
Aklesh yadav <110539247+assistanceseries123@users.noreply.github.com>
2024-01-27 23:51:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle
+
+#loading the pickle file
+pipe=pickle.load(open('pipe.pkl','rb'))
+teams = ['Sunrisers Hyderabad',
+ 'Mumbai Indians',
+ 'Royal Challengers Bangalore',
+ 'Kolkata Knight Riders',
+ 'Kings XI Punjab',
+ 'Chennai Super Kings',
+ 'Rajasthan Royals',
+ 'Delhi Capitals']
+
+cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
+       'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
+       'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
+       'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
+       'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
+       'Sharjah', 'Mohali', 'Bengaluru']
+
+col1,col2=st.columns(2)
+with col1:
+    batting_team=st.selectbox("select the batting team",sorted(teams))
+with col2:
+    bowling_team=st.selectbox("select the bowling team",sorted(teams))
+
+selected_city=st.selectbox("select the host city",sorted(cities))
+
+target=st.number_input("Target")
+
+col3,col4,col5=st.columns(3)
+with col3:
+    score=st.number_input("score")
+with col4:
+    over=st.number_input("Overs Completed")
+with col5:
+    wickets=st.number_input("wickets out")
+
+if st.button('Predict Probability'):
+    runs_left = target - score
+    balls_left = 120 - (over*6)
+    wickets = 10 - wickets
+    crr = score/over
+    rrr = (runs_left*6)/balls_left
+
+    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
+
+    result = pipe.predict_proba(input_df)
+    loss = result[0][0]
+    win = result[0][1]
+    st.header(batting_team + "- " + str(round(win*100)) + "%")
+    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
